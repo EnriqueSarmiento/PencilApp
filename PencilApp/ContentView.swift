97a58917795cbc188987fba703318a007d721343
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-10 16:33:16 -0400

    FEATURE: integrated base canvas with Pencilkit

--- PencilApp/ContentView.swift
+++ PencilApp/ContentView.swift
@@ -9,15 +9,31 @@ import SwiftUI
 import PencilKit
 
 struct ContentView: View {
+   
+   @State private var canvas = PKCanvasView()
+   
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+       NavigationView{
+          Canvas(canvas: $canvas)
+             .navigationTitle("Dibujo")
+             .navigationBarTitleDisplayMode(.inline)
+             
+       }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 
+struct Canvas: UIViewRepresentable {
+   
+   @Binding var canvas: PKCanvasView
+   
+   func makeUIView(context: Context) -> some PKCanvasView {
+      /** here we stablished the drawing policy, basically stated that can be drawing with apple pencil or tap gesture with fingers, if we select .pecilOnly then this will only work on an ipad, otherwise we could use it on iPhone too.  */
+      canvas.drawingPolicy = .anyInput
+      return canvas
+   }
+   
+   func updateUIView(_ uiView: UIViewType, context: Context) {
+
+   }
+}
