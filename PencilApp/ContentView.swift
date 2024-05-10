ad2f7a8257fce9935ff575eca51014eff2f47a27
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-10 16:11:56 -0400

    Initial Commit

---
+++ PencilApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PencilApp
+//
+//  Created by Enrique Sarmiento on 10/5/24.
+//
+
+import SwiftUI
+import PencilKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
---
+++ PencilApp/PencilAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilAppApp.swift
+//  PencilApp
+//
+//  Created by Enrique Sarmiento on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PencilAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
