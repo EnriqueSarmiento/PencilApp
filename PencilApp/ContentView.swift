ed0ef56df5679e5f1ebb0b5d5c7c157f5cd4bb2d
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-10 17:22:33 -0400

    FEATURE: added eraser tool and earse all content on canvas

--- PencilApp/ContentView.swift
+++ PencilApp/ContentView.swift
@@ -14,11 +14,12 @@ struct ContentView: View {
    /** varriable to allow user to select a tool type and color*/
    @State private var color: Color = .black
    @State private var type : PKInkingTool.InkType = .pencil
+   @State private var isDrawing: Bool = true
    
     var body: some View {
        NavigationView{
 //          Canvas(canvas: $canvas, color: $color, type: $type)
-          Canvas($canvas, $color, $type)
+          Canvas($canvas, $color, $type, $isDrawing)
              .navigationTitle("Dibujo")
              .navigationBarTitleDisplayMode(.inline)
              .toolbar{
@@ -34,7 +35,13 @@ struct ContentView: View {
                    ButtonView(action: {
                       type = .marker
                    }, icon: "highlighter")
-                   
+                   ButtonView(action: {
+                      isDrawing.toggle()
+                   }, icon: "pencil.slash")
+                   /** this button ERASE ALL CONTENT*/
+                   ButtonView(action: {
+                      canvas.drawing = PKDrawing()
+                   }, icon: "trash")
                 }
              }
              
@@ -64,12 +71,14 @@ struct Canvas: UIViewRepresentable {
    @Binding var canvas: PKCanvasView
    @Binding var color: Color
    @Binding var type : PKInkingTool.InkType
+   @Binding var isDrawing: Bool
    
    /** here we declare de binding variables, THIS WAS WE DO NOT NEED TO CALL OUR STRUCT LIKE Canvas(canvas: $canvas, color: $color) BUT AS Canvas($canvas, $color) this will work for anything like view Models, functions, etc*/
-   init(_ canvas: Binding<PKCanvasView>, _ color: Binding<Color>, _ type: Binding<PKInkingTool.InkType>){
+   init(_ canvas: Binding<PKCanvasView>, _ color: Binding<Color>, _ type: Binding<PKInkingTool.InkType>, _ isDrawing: Binding<Bool>){
      _canvas = canvas
      _color = color
      _type = type
+      _isDrawing = isDrawing
    }
    
    /** here we define our type of pencil or tool for our canvas*/
@@ -78,17 +87,20 @@ struct Canvas: UIViewRepresentable {
       PKInkingTool(type, color: UIColor(color))
    }
    
+   /** to earese content */
+   let eraser = PKEraserTool(.bitmap)
+   
    func makeUIView(context: Context) -> some PKCanvasView {
       /** here we stablished the drawing policy, basically stated that can be drawing with apple pencil or tap gesture with fingers, if we select .pecilOnly then this will only work on an ipad, otherwise we could use it on iPhone too.  */
       canvas.drawingPolicy = .anyInput
       /** to change the type of tool or pencil we need to pass it to our canvas*/
-      canvas.tool = pencil
+      canvas.tool = isDrawing ? pencil : eraser
    
       return canvas
    }
    
    /** here we assure us that the view will update when pencil is changed*/
    func updateUIView(_ uiView: UIViewType, context: Context) {
-      uiView.tool = pencil
+      uiView.tool = isDrawing ? pencil : eraser
    }
 }
